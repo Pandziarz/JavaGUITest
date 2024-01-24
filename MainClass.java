51132ec1eefdbf2f92dc3f42decf2ac3c7774e51
Pandziarz <124568923+Pandziarz@users.noreply.github.com>
2024-01-24 19:48:43 +0100

    Class

---
+++ MainClass.java
@@ -0,0 +1,42 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MainClass {
+
+    int clicks = 0;
+
+    public static void main(String[] args) {
+        GUI();
+    }
+
+    public static void GUI() {
+        JButton button = new JButton("Click Me!");
+        button.addActionListener(this);
+
+        JLabel label = new JLabel("Number of Clicks: 0");
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+
+        JFrame frame = new JFrame();
+        frame.add(panel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Testowy Program");
+    }
+
+    @Override
+    public void action
+}
